c4d53e4c3b823d367411b9040cf14dd0ab519adc
Mordecai Yiadom <mordeyiadom@gmail.com>
2024-09-22 21:19:34 -0400

    Fix 'unknown identifier' error w/ compiling on UNIX

--- include/unisocket.h
+++ include/unisocket.h
@@ -20,7 +20,25 @@
         #define ISVALIDSOCKET(s) (s != INVALID_SOCKET)
         #define GETSOCKETERRNO() WSAGetLastError()
         #define CLOSESOCKET(s) closesocket(s)
-        #define UNISOCKET_CLEANUP() WSACleanup()     
+        #define UNISOCKET_CLEANUP() WSACleanup()
+
+        /*Define 'UNISOCKET_RETURN_WSADATA' only if you wish to use the filled WSADATA struct in your program.*/
+        #ifdef UNISOCKET_RETURN_WSADATA
+            #define UNISOCKET_STARTUP(wsaDataAddr) WSAStartup(MAKEWORD(2, 2), wsaDataAddr)
+        #else
+            #define UNISOCKET_STARTUP() unisocket_WSAStartup()
+        #endif
+
+        //Windows Functions
+
+        /*Encapsulates Winsock2's 'WSAStartup()'. Returns '0' in for unix systems. While this function is available to use directly, 
+        it is best to use the 'UNISOCKET_STARTUP()' function macro instead. This is the default 'UNICODE_STARTUP()' function macro defined.*/
+        static inline int unisocket_WSAStartup(void)
+        {
+            WSADATA wsaData;
+            return WSAStartup(MAKEWORD(2, 2), &wsaData);
+        }
+
     //UNIX COMPILATION    
     #else
         //Unix Headers
@@ -39,6 +57,7 @@
         #define ISVALIDSOCKET(s) (s >= 0)
         #define GETSOCKETERRNO() (errno)
         #define CLOSESOCKET(s) close(s)
+        #define UNISOCKET_STARTUP() 0
         #define UNISOCKET_CLEANUP() 0
     #endif
 
@@ -47,34 +66,4 @@
     #define SHUTDOWN_WR(s) shutdown(s, 1)
     #define SHUTDOWN_RDWR(s) shutdown(s, 2)
     #define SHUTDOWN(s, h) shutdown(s, h)
-
-    /*Define 'UNISOCKET_RETURN_WSADATA' only if you wish to use the filled WSADATA struct in your program.*/
-    #ifdef UNISOCKET_RETURN_WSADATA
-        #define UNISOCKET_STARTUP(wsaDataAddr) unisocket_startup_return_WSADATA(wsaDataAddr)
-    #else
-        #define UNISOCKET_STARTUP() unisocket_startup_default()
-    #endif
-
-    /*Encapsulates Winsock2's 'WSAStartup()'. Returns '0' in for unix systems. While this function is available to use directly, 
-    it is best to use the 'UNISOCKET_STARTUP(wsaDataAddr)' function macro instead. To use 'UNISOCKET_STARTUP(wsaDataAddr)', define 'UNISOCKET_RETURN_WSADATA' before including 'unisocket.h'.*/
-    static inline int unisocket_startup_return_WSADATA(WSADATA *wsaData)
-    {   
-        #if defined _WIN32
-            return WSAStartup(MAKEWORD(2, 2), wsaData);
-        #else
-            return 0;        
-        #endif
-    }
-
-    /*Encapsulates Winsock2's 'WSAStartup()'. Returns '0' in for unix systems. While this function is available to use directly, 
-    it is best to use the 'UNISOCKET_STARTUP()' function macro instead. This is the default 'UNICODE_STARTUP()' function macro defined.*/
-    static inline int unisocket_startup_default(void)
-    {
-        #if defined _WIN32
-            WSADATA wsaData;
-            return WSAStartup(MAKEWORD(2, 2), &wsaData);
-        #else
-            return 0;        
-        #endif
-    }
  #endif   
