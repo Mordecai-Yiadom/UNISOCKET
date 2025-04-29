ccb7d8f3c4cec6d5bca0015e4698037b57326612
Mordecai Yiadom <mordeyiadom@gmail.com>
2025-04-29 19:58:58 -0400

    Refactor comments

--- include/unisocket.h
+++ include/unisocket.h
@@ -18,25 +18,37 @@
         #ifdef _MSC_VER
             #pragma comment(lib, "ws2_32.lib")
         #endif   
-
+    
         //Windows Macros
-        #define UNISOCKET_WINDOWS //Easy way to check what platform program is compiled on
+ 
+        /*Defined only if compiled on Windows systems. Use this to check what if you are compiling on Windows or not.*/
+        #define UNISOCKET_WINDOWS
 
+        /*Abstracts away Windows's 'INVALID_SOCKET' macro for sockets. On Windows, this macro evaluates to true if: 's != INVALID_SOCKET'.
+        On Unix systems, this macro evaluates true if: 's >= 0' */
         #define ISVALIDSOCKET(s) (s != INVALID_SOCKET)
+
+        /*Absracts away Windows's 'WSAGetLastError()'. On Unix systems this macro abstracts 'errno'.*/
         #define GETSOCKETERRNO() WSAGetLastError()
+
+        /*Abstracts away Windows's 'closesocket()'. On Unix systems, this macro absracts the socket operation 'close()'*/
         #define CLOSESOCKET(s) closesocket(s)
+
+        /*Abstracts Windows's 'WSACleanup()'. Returns '0' in for Unix systems. While this function is available to use directly, 
+        it is best to use the 'UNISOCKET_CLEANUP()' function macro instead. This is the default 'UNISOCKET_CLEANUP()' function macro defined.*/
         #define UNISOCKET_CLEANUP() WSACleanup()
 
         /*Define 'UNISOCKET_RETURN_WSADATA' only if you wish to use the filled WSADATA struct in your program.*/
         #ifdef UNISOCKET_RETURN_WSADATA
             #define UNISOCKET_STARTUP(wsaDataAddr) WSAStartup(MAKEWORD(2, 2), wsaDataAddr)
         #else
+            /*Abstracts Windows's 'WSAStartup()' function. This must be called before any socket operations happen.*/
             #define UNISOCKET_STARTUP() UNISOCKET_WSAStartup()
         #endif
 
         //Windows Functions
 
-        /*Encapsulates Winsock2's 'WSAStartup()'. Returns '0' in for unix systems. While this function is available to use directly, 
+        /*Abstracts Windows's 'WSAStartup()'. Returns '0' in for Unix systems. While this function is available to use directly, 
         it is best to use the 'UNISOCKET_STARTUP()' function macro instead. This is the default 'UNISOCKET_STARTUP()' function macro defined.*/
         static inline int UNISOCKET_WSAStartup(void)
         {
@@ -56,20 +68,44 @@
         #include <errno.h>
 
         //Unix Macros
-        #define UNISOCKET_UNIX  //Easy way to check what platform program is compiled on  
 
+        /*Defined only if compiled on Unix systems. Use this to check what if you are compiling on a Unix system or not.*/ 
+        #define UNISOCKET_UNIX   
+
+        /*Abstracts the 'SOCKET' typedef on Windows. SOCKET when on compiling on Unix is a signed int.*/
         #define SOCKET int
+
+        /*Abstracts away Windows's 'INVALID_SOCKET' macro for sockets. On Windows, this macro evaluates to true if: 's != INVALID_SOCKET'.
+        On Unix systems, this macro evaluates true if: 's >= 0' */
         #define ISVALIDSOCKET(s) (s >= 0)
+
+        /*Absracts away Windows's 'WSAGetLastError()'. On Unix systems this macro abstracts 'errno'.*/
         #define GETSOCKETERRNO() (errno)
+
+        /*Abstracts away Windows's 'closesocket()'. On Unix systems, this macro absracts the socket operation 'close()'*/
         #define CLOSESOCKET(s) close(s)
+
+        /*Abstracts Windows's 'WSAStartup()'. Returns '0' in for Unix systems. While this function is available to use directly, 
+        it is best to use the 'UNISOCKET_STARTUP()' function macro instead. This is the default 'UNISOCKET_STARTUP()' function macro defined.*/
         #define UNISOCKET_STARTUP() 0
+
+        /*Abstracts Windows's 'WSACleanup()'. Returns '0' in for Unix systems. While this function is available to use directly, 
+        it is best to use the 'UNISOCKET_CLEANUP()' function macro instead. This is the default 'UNISOCKET_CLEANUP()' function macro defined.*/
         #define UNISOCKET_CLEANUP() 0
     #endif
 
     //Windows and Unix Macros
+
+    /*Abstracts 'shutdown()' for both Windows and Unix systems. It closes the passed socket for reading. */
     #define SHUTDOWN_RD(s) shutdown(s, 0)
+
+    /*Abstracts 'shutdown()' for both Windows and Unix systems. It closes the passed socket for writing. */
     #define SHUTDOWN_WR(s) shutdown(s, 1)
+
+    /*Abstracts 'shutdown()' for both Windows and Unix systems. It closes the passed socket for reading and writing. */
     #define SHUTDOWN_RDWR(s) shutdown(s, 2)
+
+    //Set for removal. Do not use.
     #define SHUTDOWN(s, h) shutdown(s, h)
     
  #endif   
